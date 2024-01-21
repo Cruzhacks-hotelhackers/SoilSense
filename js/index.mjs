db02f3def432d8fdb15e9a552f6d1abca4a648ab
Luke Bair <lukebair@me.com>
2024-01-21 01:42:15 -0800

    fun

--- js/index.mjs
+++ js/index.mjs
@@ -1,14 +1,14 @@
-import OpenAI from 'openai';
+import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: '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' // This is the default and can be omitted
-});
+const openai = new OpenAI();
 
 async function main() {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test' }],
-    model: 'gpt-3.5-turbo',
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
   });
+
+  console.log(completion.choices[0]);
 }
 
 main();
